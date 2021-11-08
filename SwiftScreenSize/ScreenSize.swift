888d7cb5d3614e7479eaabd14a4d5192da03262d
paige <paige@paiges-Mac-mini.local>
2021-11-08 22:59:19 +0900

    update

--- SwiftScreenSize/ScreenSize.swift
+++ SwiftScreenSize/ScreenSize.swift
@@ -27,10 +27,9 @@ struct ScreenSize {
 
 struct DeviceType {
     
-    static let IPHONE_SIZE: [CGFloat] = [568, 667, 736, 812, 896]
-    static let IPAD_SIZE: [CGFloat] = [1024, 1080, 1112, 1194, 1366]
-    
+    static let IPHONE_SIZE: [CGFloat] = [568, 667, 736, 812, 844, 896, 926]
     static let IPHONE_4_OR_LESS: Bool  = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.SCREEN_MAX_HEIGHT < 568
+    static let IPOD_TOUCH: Bool = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.SCREEN_MAX_HEIGHT == 568
     static let IPHONE_SE_1TH: Bool = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.SCREEN_MAX_HEIGHT == 568
     static let IPHONE_SE_2TH: Bool = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.SCREEN_MAX_HEIGHT == 667
     static let IPHONE_5S: Bool = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.SCREEN_MAX_HEIGHT == 568
@@ -43,18 +42,40 @@ struct DeviceType {
     static let IPHONE_8: Bool = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.SCREEN_MAX_HEIGHT == 667
     static let IPHONE_8_PLUS: Bool = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.SCREEN_MAX_HEIGHT == 736
     static let IPHONE_X: Bool = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.SCREEN_MAX_HEIGHT == 812
+    static let IPHONE_XS: Bool = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.SCREEN_MAX_HEIGHT == 812
+    static let IPHONE_XR: Bool = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.SCREEN_MAX_HEIGHT == 896
     static let IPHONE_11: Bool = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.SCREEN_MAX_HEIGHT == 896
     static let IPHONE_11_PRO: Bool = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.SCREEN_MAX_HEIGHT == 812
     static let IPHONE_11_PRO_MAX: Bool = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.SCREEN_MAX_HEIGHT == 896
+    static let IPHONE_12: Bool = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.SCREEN_MAX_HEIGHT == 844
+    static let IPHONE_12_PRO: Bool = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.SCREEN_MAX_HEIGHT == 844
+    static let IPHONE_12_PRO_MAX: Bool = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.SCREEN_MAX_HEIGHT == 926
+    static let IPHONE_12_MINI: Bool = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.SCREEN_MAX_HEIGHT == 812
+    static let IPHONE_13: Bool = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.SCREEN_MAX_HEIGHT == 844
+    static let IPHONE_13_PRO: Bool = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.SCREEN_MAX_HEIGHT == 844
+    static let IPHONE_13_PRO_MAX: Bool = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.SCREEN_MAX_HEIGHT == 926
+    static let IPHONE_13_MINI: Bool = UIDevice.current.userInterfaceIdiom == .phone && ScreenSize.SCREEN_MAX_HEIGHT == 812
+    
+    
+    static let IPAD_SIZE: [CGFloat] = [1024, 1080, 1180, 1112, 1133, 1194, 1366]
+    
+    static let IPAD_MINI_45H: Bool = UIDevice.current.userInterfaceIdiom == .pad && ScreenSize.SCREEN_MAX_HEIGHT == 1024
+    static let IPAD_MINI_6TH: Bool = UIDevice.current.userInterfaceIdiom == .pad && ScreenSize.SCREEN_MAX_HEIGHT == 1133
     static let IPAD_5TH: Bool = UIDevice.current.userInterfaceIdiom == .pad && ScreenSize.SCREEN_MAX_HEIGHT == 1024
     static let IPAD_7TH: Bool = UIDevice.current.userInterfaceIdiom == .pad && ScreenSize.SCREEN_MAX_HEIGHT == 1080
+    static let IPAD_9TH: Bool = UIDevice.current.userInterfaceIdiom == .pad && ScreenSize.SCREEN_MAX_HEIGHT == 1080
     static let IPAD_AIR: Bool = UIDevice.current.userInterfaceIdiom == .pad && ScreenSize.SCREEN_MAX_HEIGHT == 1024
+    static let IPAD_AIR_2TH: Bool = UIDevice.current.userInterfaceIdiom == .pad && ScreenSize.SCREEN_MAX_HEIGHT == 1024
     static let IPAD_AIR_3TH: Bool = UIDevice.current.userInterfaceIdiom == .pad && ScreenSize.SCREEN_MAX_HEIGHT == 1112
+    static let IPAD_AIR_4TH: Bool = UIDevice.current.userInterfaceIdiom == .pad && ScreenSize.SCREEN_MAX_HEIGHT == 1180
     static let IPAD_AIR_2: Bool = UIDevice.current.userInterfaceIdiom == .pad && ScreenSize.SCREEN_MAX_HEIGHT == 1024
     static let IPAD_PRO_9_7: Bool = UIDevice.current.userInterfaceIdiom == .pad && ScreenSize.SCREEN_MAX_HEIGHT == 1024
     static let IPAD_PRO_10_5: Bool = UIDevice.current.userInterfaceIdiom == .pad && ScreenSize.SCREEN_MAX_HEIGHT == 1112
     static let IPAD_PRO_11_2TH: Bool = UIDevice.current.userInterfaceIdiom == .pad && ScreenSize.SCREEN_MAX_HEIGHT == 1194
+    static let IPAD_PRO_11_3TH: Bool = UIDevice.current.userInterfaceIdiom == .pad && ScreenSize.SCREEN_MAX_HEIGHT == 1194
     static let IPAD_PRO_12_9: Bool = UIDevice.current.userInterfaceIdiom == .pad && ScreenSize.SCREEN_MAX_HEIGHT == 1366
     static let IPAD_PRO_12_9_2TH: Bool = UIDevice.current.userInterfaceIdiom == .pad && ScreenSize.SCREEN_MAX_HEIGHT == 1366
     static let IPAD_PRO_12_9_4TH: Bool = UIDevice.current.userInterfaceIdiom == .pad && ScreenSize.SCREEN_MAX_HEIGHT == 1366
+    static let IPAD_PRO_12_9_5TH: Bool = UIDevice.current.userInterfaceIdiom == .pad && ScreenSize.SCREEN_MAX_HEIGHT == 1366
 }
+
--- SwiftScreenSize/ViewController.swift
+++ SwiftScreenSize/ViewController.swift
@@ -10,9 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var label: UILabel = {
+        return UILabel(frame: UIScreen.main.bounds)
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.addSubview(label)
+        label.textColor = .black
+        label.textAlignment = .center
+        label.text = "\(UIScreen.main.bounds.height)"
+        print(UIScreen.main.bounds.height)
+        
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            print("This is a phone")
+        }
+        
     }
 
 
